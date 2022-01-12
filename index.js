a9cded350976a1b4e9653f85ba3da9135da897cf
Vidyesh Kumar <86671120+vidyesh-kumar@users.noreply.github.com>
2022-01-12 23:55:25 +0530

    Responsive Table Added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const projectName = 'tribute-page';
+
+function MOver(id)
+{   for(let i = 0; i<document.getElementsByClassName(id).length;i++){
+    document.getElementsByClassName(id)[i].style.fontWeight="bold";
+}
+}
+function MOut(id)
+{   for(let i = 0; i<document.getElementsByClassName(id).length;i++){
+    document.getElementsByClassName(id)[i].style.fontWeight="normal";
+}
+}
